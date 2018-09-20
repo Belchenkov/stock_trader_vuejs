641c02b4716d41937639dfe20a97e5568fa23a69
Aleksey Belchenkov <u608110@gmail.com>
2018-09-20 07:58:19 +0300

    Create Stock Components

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>The Stock component</h1>
+    <div class="col-sm-6 col-md-4 mb-5">
+      <div class="card">
+        <div class="card-header bg-success">
+          <h5 class="card-title text-white">
+              NAME
+              <small>(Price: PRICE)</small>  
+            </h5>
+        </div>
+        <div class="card-body d-flex justify-content-between">
+            <div>
+              <input 
+                type="number"
+                class="form-control"
+                placeholder="Quantity"
+              >
+            </div>
+            <div>
+              <button
+                class="btn btn-success"
+              >Buy</button>
+            </div>
+          </div>
+      </div>
+    </div>
 </template>
