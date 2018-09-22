9e3d143b809c522236f05b3b17ace6d267df10ea
Aleksey Belchenkov <u608110@gmail.com>
2018-09-22 15:03:48 +0300

    Working on the Portfolio Stocks

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -1,3 +1,62 @@
 <template>
-  <h1>The Portfolio Stock component</h1>
+    <div class="col-sm-6 col-md-4 mb-5">
+      <div class="card">
+        <div class="card-header bg-info">
+          <h5 class="card-title text-white">
+              {{ stock.name }}
+              <small>(Price: {{ stock.price }} | Quantity: {{ stock.quantity }})</small>  
+            </h5>
+        </div>
+        <div class="card-body d-flex justify-content-between">
+            <div>
+              <input 
+                type="number"
+                class="form-control"
+                placeholder="Quantity"
+                v-model="quantity"
+              >
+            </div>
+            <div>
+              <button
+                class="btn btn-success"
+                @click="sellStock"
+                :disabled="quantity <= 0"
+              >Sell</button>
+            </div>
+          </div>
+      </div>
+    </div>
 </template>
+
+<script>
+  import { mapActions } from 'vuex';
+
+  export default {
+    props: [
+      'stock'
+    ],
+    data () {
+      return {
+        quantity: 0
+      }
+    },
+    methods: {
+     ...mapActions([
+          'sellStock'
+     ]), 
+     sellStock () {
+       const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+       };
+       this.sellStock();
+       
+     }
+    }
+  }
+</script>
+
+<style>
+ 
+</style>
